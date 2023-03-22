102308496346c7bb029fe374c4a9b279b64583b4
Evgeniy Neronov <eneronov@project-llc.ru>
2023-03-22 10:08:13 +0300

    add project test ata generator

---
+++ test_data_generator/Program.cs
@@ -0,0 +1,27 @@
+﻿using Domain;
+using MyApp.Infrastructure;
+
+namespace test_data_generator;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var contextType = typeof(MyDbContext);
+        var myDbContext = (MyDbContext)Activator.CreateInstance(contextType, DbConfiguration.CreateOptions("127.0.0.1", "my_postgis"));
+
+        var userSet = myDbContext.Set<User>();
+
+        userSet.Add(new User()
+        {
+            Id = Guid.NewGuid(),
+            Latitude = 4,
+            Longitude = 3,
+            Name = "ConsoleUser"
+        });
+
+        myDbContext.SaveChanges();
+
+        Console.WriteLine("Hello, World!");
+    }
+}
--- web/distance_query_example.sql
+++ web/distance_query_example.sql
@@ -1,3 +1,4 @@
+/*
 CREATE EXTENSION cube;
 CREATE EXTENSION earthdistance;
 
@@ -7,3 +8,4 @@ USING GIST (ll_to_earth(latitude, longitude));
 
 SELECT ST_Distance(location, ST_GeographyFromText('POINT(-74.044635 40.689252)')) FROM public.facility;
 SELECT earth_distance(ll_to_earth(40.689252,-74.044635), ll_to_earth(latitude,longitude)) FROM public.facility;
+*/
